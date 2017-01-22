da43fb5a3fd4f497752bc83277f6892a13588247
Joe Haggerty <joemhaggerty@gmail.com>
2017-01-22 00:59:29 -0500

    Implemented output of Fibonacci Sequence to a table

--- lab1/fib.js
+++ lab1/fib.js
@@ -1,26 +1,51 @@
+var fib_arr = [];
+
 function fib_val(){
 	var inStr = document.getElementById("in_textbox").value;
 	var inInt = parseInt(inStr);
 	
+	//Check for positive integer inputs
 	if(isNaN(inInt)){
-		out.innerHTML = "Invalid Input";
+		out.innerHTML = "<b>Invalid Input</b>";
 		return;
 	}
 	else if(inInt < 0){
-		out.innerHTML = "Cannot compute Fib of a negative integer";
+		out.innerHTML = "<b>Cannot compute Fib of a negative integer</b>";
 		return;
 	}
 	else{
+		var prev = 0, next = 1, res = 0;
 		if(inInt == 0 || inInt == 1){
 			out.innerHTML = inInt.toString();
-			return;
+			if(inInt == 0){
+				fib_arr.push(0);
+			}
+			else{
+				fib_arr.push(0);
+				fib_arr.push(1);
+			}
+			out.innerHTML = "<p>Fibonacci Value: " + inInt.toString() + "</p>\n" + fib_table();
 		}
-		var prev = 0, next = 1, res = 0;
-		for(i=1; i<inInt; i++){
-			res = prev+next;
-			prev = next;
-			next = res;
+		else{
+			fib_arr.push(0);
+			fib_arr.push(1);
+			for(i=1; i<inInt; i++){
+				res = prev+next;
+				fib_arr.push(res);
+				prev = next;
+				next = res;
+			}
+			out.innerHTML = "<p>Fibonacci Value: " + res.toString() + "</p>\n" + fib_table();
 		}
-		out.innerHTML = res.toString();
+		fib_arr = [];
 	}
+}
+
+function fib_table(){
+	var htmlStr = "<table border='1'><tr><th>n</th><th>fib(n)</th></tr>";
+	for(i=0; i<fib_arr.length; i++){
+		htmlStr += "<tr><td>" + i.toString() + "</td><td>" + fib_arr[i].toString() + "</td></tr>";
+	}
+	htmlStr += "</table>"
+	return htmlStr;
 }
